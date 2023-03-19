477853b3f732fec68828beb43cfbd4a2b60dc77c
Uday Kumar J B <97491777+UdayReddyJB@users.noreply.github.com>
2023-03-19 15:50:02 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; <?php echo date('Y');?> E-Learning Course Registration|HCLTECH - Internship on FSWD | Coder & Design Project By : <a href="https://chat.whatsapp.com/L0v5enIYE2x4w4XhC9nAUl" target="_blank">UDAY KUMAR J B & THARUN P C: ELCR chatbot👨🏻‍💻👨🏻‍💻</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
